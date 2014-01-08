a85dba40243d53e9c5642e7bc06d81438257de26
matthew <matthew@matthewstout>
2014-01-08 11:11:48 -0700

    gallons to liters Matthew Stout

---
+++ src/literstogallons/galtolit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class galtolit {
+ /*
+        project 1-1
+        this program converts gallons to liters
+        Call this program GalToLit.java
+        */
+   
+    public static void main(String[] args) {
+       double gallon;
+       double liter;
+       gallon = 1; //start with 10 gallons
+       liter = gallon * 3.7854;
+        
+       System.out.println(gallon + " gallons is " + liter + " liters.");
+        
+    }
+}
